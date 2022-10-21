47cb49e193c77a715ef13e045b66b0e86a292cbf
Chris Searle <chris@chrissearle.org>
2022-10-21 21:17:45 +0200

    End of video11

--- Player/Player.gd
+++ Player/Player.gd
@@ -20,7 +20,7 @@ onready var animation_state = animation_tree.get("parameters/playback")
 func _ready():
 	animation_tree.active = true
 
-func _process(delta):
+func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
--- World/Grass.gd
+++ World/Grass.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var grass_effect_scene = load("res://Effects/GrassEffect.tscn")
-		var grass_effect = grass_effect_scene.instance()
-		
-		var world = get_tree().current_scene
-		
-		world.add_child(grass_effect)
-		grass_effect.global_position = global_position
-		
-		queue_free()
+
+func _on_HurtBox_area_entered(area):
+	create_grass_effect()
+	queue_free()
+
+func create_grass_effect():
+	var grass_effect_scene = load("res://Effects/GrassEffect.tscn")
+	var grass_effect = grass_effect_scene.instance()
+	
+	var world = get_tree().current_scene
+	
+	world.add_child(grass_effect)
+	grass_effect.global_position = global_position
