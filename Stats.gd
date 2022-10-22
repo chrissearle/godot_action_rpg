cb8c3f0aa78c42739bd11802eb5b3ab0e79253c3
Chris Searle <chris@chrissearle.org>
2022-10-22 13:32:32 +0200

    End of video18

--- Stats.gd
+++ Stats.gd
@@ -1,12 +1,26 @@
 extends Node
 
 signal no_health
+signal health_changed(value)
+signal max_health_changed(value)
 
-export(int) var max_health = 1
+export(int) var max_health = 1 setget set_max_health
 onready var health = max_health setget set_health
 
+func set_max_health(value):
+	max_health = value
+	
+	if health != null:
+		self.health = min(health, max_health)
+
+	emit_signal("max_health_changed", max_health)
+	
 func set_health(value):
 	health = value
-	
+
+	emit_signal("health_changed", health)
+		
 	if health <= 0:
 		emit_signal("no_health")
+
+
